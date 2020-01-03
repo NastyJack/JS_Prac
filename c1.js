80422cd73510098d3d34864645f9f4bab90e5ca9
Rishabh Nishad <44137857+NastyJack@users.noreply.github.com>
2020-01-03 16:36:49 +0530

    Update c1.js

--- c1.js
+++ c1.js
@@ -13,8 +13,20 @@ Program to take console input.
 
 var rl = require('readline');
 var prompts = rl.createInterface(process.stdin,process.stdout);
-prompts.question("\n Enter a num ",function(val){
-    var tmp="";
+
+function menu(){
+    prompts.question("\n Enter a num ",function(...val)
+    {
+     
+    }
+}
+
+
+function pq_num(){    
+prompts.question("\n Enter a num ",function(...val){
+   
+    
+    var tmp=[""];
     if(val>5&&val>0)
     tmp="\n The num is bw 0 and 5";
 
@@ -29,4 +41,28 @@ prompts.question("\n Enter a num ",function(val){
     console.log(tmp+"\n");
     process.exit();
 
+
 });
+
+}
+
+arr=[2,3,5,6,23,6,12];
+
+
+arr.forEach(dist10);
+
+function dist10(val,index)
+{ // console.log(val);
+    console.log(arr[arr.length-1]);
+     arr.length=arr.length-1;
+
+}
+
+
+
+function dist(val,index)
+{ 
+    console.log(val);
+}
+
+
